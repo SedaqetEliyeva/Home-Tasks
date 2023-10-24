b0acec5c7431e911483ae1a2455750458f66c55b
Sedaqet Eliyeva <sedaqet.eliyeva04@gmail.com>
2023-10-24 16:43:13 +0400

    task1

---
+++ index3.js
@@ -0,0 +1,15 @@
+const firstNumber=2;
+const secondNumber=3;
+const thirdNumber=5;
+
+if(firstNumber>secondNumber && firstNumber>thirdNumber) {
+    console.log(firstNumber);
+}
+
+else if(secondNumber>firstNumber && secondNumber>thirdNumber) {
+    console.log(secondNumber);
+}
+
+else{
+    console.log(thirdNumber);
+}
